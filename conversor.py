976b6806e3a3aab6fa98d9011054a999a31f187a
Vangi <dvangioni@gmail.com>
2020-12-15 22:21:56 -0300

    Feature distintas monedas

--- conversor.py
+++ conversor.py
@@ -1,10 +1,38 @@
-# Config Valor Dolar
-valor_dolar = 82.49
-# Código
-pesos = input("¿Cuántos pesos argentinos tienes?: ")
-pesos = float(pesos)
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
+# Config
+menu = """
+Bienvenido al conversor de monedas 💰
+1 - Pesos Argentinos
+2 - Pesos Colombianos
+3 - Pesos Mexicanos
 
-print("Tienes U$S " + dolares + " dólares")
+Elige una opción: """
+
+# Código
+opcion = input(menu)
+
+if opcion == '1':
+    pesos = input("¿Cuántos pesos Argentinos tienes?: ")
+    valor_dolar = 82.49
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes U$S " + dolares + " dólares")
+elif opcion == '2':
+    pesos = input("¿Cuántos pesos Colombianos tienes?: ")
+    valor_dolar = 3419.70
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes U$S " + dolares + " dólares")
+elif opcion == '3':
+    pesos = input("¿Cuántos pesos Mexicanos tienes?: ")
+    valor_dolar = 19.95
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes U$S " + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta")
