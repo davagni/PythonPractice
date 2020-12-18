05c764a5f2c49ae2f35d11b563d2813ac4cf5182
Vangi <dvangioni@gmail.com>
2020-12-18 15:32:43 -0300

    break dentro de for, pruebas en while

--- break_continue.py
+++ break_continue.py
@@ -1,8 +1,28 @@
 def run():
-    for contador in range(1000):
-        if contador % 2 != 0:
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+
+    # for i in range(10000):
+    #     print(i)
+    #     if i == 5678:
+    #         break
+
+    # texto = input("Escribe un texto: ")
+    # for letra in texto:
+    #     if letra == 'o':
+    #         break
+    #     print(letra)
+
+    numero = 0
+    while numero < 100:
+        numero += 1
+        if numero % 2 != 0:
             continue
-        print(contador)
+        elif numero == 50:
+            break
+        print(numero)
 
 
 if __name__ == "__main__":
