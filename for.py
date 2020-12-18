a3c47f18444255fda0b23b144862e8579a51a882
Vangi <dvangioni@gmail.com>
2020-12-17 21:24:43 -0300

    bucle for

---
+++ for.py
@@ -0,0 +1,15 @@
+def run():
+    # contador = 0
+    # while contador < 1000:
+    #     contador += 1
+    #     print(contador)
+
+    # for contador in range(1, 1001, 100):
+    #     print(contador)
+
+    for i in range(11):
+        print(i * 11)
+
+
+if __name__ == '__main__':
+    run()
